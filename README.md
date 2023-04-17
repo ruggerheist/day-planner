7682d01188f15343f7120a8a4dfc2e0c1071f04e
Rick Heist <rick.heist@gmail.com>
2023-04-16 22:29:53 -0400

    finished light styling, tested application, and finished README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# day-planner
+# DAY PLANNER
+
+## Descrption
+A daily planner to keep a users schedule for the work day. Activities are color coded by time of day. Gray blocks are past events, the red block is the present events, and blocks in green are upcoming. JQuery was the preferred language of this project.
+
+## Usage
+The user may simply input their acitivity to the corresponding hour and click the save button associated with that hour. Storage is perpetual and can be changed daily because the time and date will automatically update to the current day and time. 
+
+![alt text](./assets/Screenshot%202023-04-16%20222259.png)
+
+### Citations
+For this project, I imported DayJS for a time and date keeping interface. Bootstrap was already linked and availble with the code provided to start this application.
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentTime = dayjs();
 let currentHour = parseInt(currentTime.format('HH'));
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-$('#currentDay').text(currentTime.format('MMMM DD, YYYY HH:mm')); 
+$('#currentDay').text(currentTime.format('MMMM DD, YYYY hh:mm a')); 
 
 hours.forEach( function(hour){
     let id = `#hour-${hour}`;    
@@ -22,26 +22,4 @@ hours.forEach( function(hour){
 $(".saveBtn").on('click', function () {
   var text = $(this).siblings(".description").val();
   localStorage.setItem($(this).parent().attr("id"), text);
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //jQuesy Each
-
-  // TODO: This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage.How might the id be
-  // useful when saving the description in local storage?
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  
 });
